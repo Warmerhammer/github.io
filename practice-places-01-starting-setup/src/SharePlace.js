cc2156b81de60840fea7b50a71fac14cc81ea098
Warmerhammer <rodonnell@koffassociates.com>
2020-10-19 12:32:13 -0700

    adding Practice Places project 10 19 20

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,10 +1,49 @@
+import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
+import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector('form');
     const locateUserBtn = document.getElementById('locate-btn');
+    this.shareBtn = document.getElementById('share-btn');
 
-    locateUserBtn.addEventListener('click', this.locateUserHandler);
-    addressForm.addEventListener('submit', this.findAddressHandler);
+    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener('click', this.sharePlaceHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+  }
+
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render(coordinates);
+      return;
+    } else {
+      this.map = new Map(coordinates);
+    }
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById('share-link');
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
+  }
+
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById('share-link');
+    if (!navigator.clipboard) {
+      sharedLinkInputElement.select();
+      return;
+    }
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert('Copied into clipboard');
+      })
+      .catch((err) => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
   }
 
   locateUserHandler() {
@@ -14,15 +53,22 @@ class PlaceFinder {
       );
       return;
     }
+
+    const modal = new Modal('loading-modal-content', 'Loading location...');
+    modal.show();
+
     navigator.geolocation.getCurrentPosition(
-      (successResult) => {
+      async (successResult) => {
         const coordinates = {
           lat: successResult.coords.latitude,
-          lng: successResult.coords.longitude
+          lng: successResult.coords.longitude,
         };
-        console.log(coordinates);
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
       },
       (error) => {
+        modal.hide();
         alert(
           'Could not locate you unfortunately. Please enter an address manually'
         );
@@ -30,7 +76,23 @@ class PlaceFinder {
     );
   }
 
-  findAddressHandler() {}
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if (!address || address.trim().length === 0) {
+      alert('Invalid address entered - please try again!');
+      return;
+    }
+    const modal = new Modal('loading-modal-content', 'Loading location...');
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
 }
 
 new PlaceFinder();
