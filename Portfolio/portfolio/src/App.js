d5ce2797e00cb2e6c4934418889ea2613431d546
Warmerhammer <rodonnell@koffassociates.com>
2021-01-22 12:19:19 -0800

    added .nojekyll 01 22 21

--- Portfolio/portfolio/src/App.js
+++ Portfolio/portfolio/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import {Container, Divider} from 'semantic-ui-react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Container, Divider } from 'semantic-ui-react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Home from './components/home';
 
 function App() {
   return (
     <Container>
       <br />
       <Router>
+        <Divider />
 
-      <Divider /> 
-
-      <div className="App">
-        <p>Hello</p>
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+  
+        </Switch>
       </Router>
     </Container>
   );
